5e4ae4be9dbe41f50d23190e93c4415fff2aabb3
bytadit <bgsp_aditya@student.uns.ac.id>
2024-02-05 16:06:17 +0700

    update npm start

--- Backend-JS-Auth/start.js
+++ Backend-JS-Auth/start.js
@@ -14,46 +14,66 @@ const seedersPath = path.join(__dirname, "api", "seeders");
 try {
   execSync(`npx sequelize db:create --config "${sequelizeConfigPath}"`);
   console.log("Database created successfully.");
+  // Run migrations if needed
+  try {
+    execSync(
+      `npx sequelize db:migrate --config "${sequelizeConfigPath}" --migrations-path "${migrationsPath}"`
+    );
+    console.log("Migrations executed successfully.");
+    try {
+      execSync(
+        `npx sequelize db:seed:all --config "${sequelizeConfigPath}" --seeders-path "${seedersPath}"`
+      );
+      console.log("Seeders executed successfully.");
+    } catch (error) {
+      console.log("Seeders not executed.");
+    }
+  } catch (error) {
+    console.error("Error running migrations:", error.stderr.toString());
+    process.exit(1); // Exit the process with an error code
+  }
 } catch (error) {
   console.log("Database already exists.");
 }
 
-// Check if migrations exist
-// Check if migrations exist
-try {
-  execSync(
-    `npx sequelize db:migrate:status --config "${sequelizeConfigPath}" --migrations-path "${migrationsPath}"`
-  );
-  console.log("Migrations exist.");
-} catch (error) {
-  if (error.status === 1) {
-    console.log("No migrations found.");
-  } else {
-    console.error("Error checking migrations:", error.stderr.toString());
-    process.exit(1); // Exit the process with an error code
-  }
-}
+require("./api/index.js");
 
-// Run migrations if needed
-try {
-  execSync(
-    `npx sequelize db:migrate --config "${sequelizeConfigPath}" --migrations-path "${migrationsPath}"`
-  );
-  console.log("Migrations executed successfully.");
-} catch (error) {
-  console.error("Error running migrations:", error.stderr.toString());
-  process.exit(1); // Exit the process with an error code
-}
+// Check if migrations exist
+// Check if migrations exist
+// try {
+//   execSync(
+//     `npx sequelize db:migrate:status --config "${sequelizeConfigPath}" --migrations-path "${migrationsPath}"`
+//   );
+//   console.log("Migrations exist.");
+// } catch (error) {
+//   if (error.status === 1) {
+//     console.log("No migrations found.");
+//   } else {
+//     console.error("Error checking migrations:", error.stderr.toString());
+//     process.exit(1); // Exit the process with an error code
+//   }
+// }
+
+// // Run migrations if needed
+// try {
+//   execSync(
+//     `npx sequelize db:migrate --config "${sequelizeConfigPath}" --migrations-path "${migrationsPath}"`
+//   );
+//   console.log("Migrations executed successfully.");
+// } catch (error) {
+//   console.error("Error running migrations:", error.stderr.toString());
+//   process.exit(1); // Exit the process with an error code
+// }
 
 // Check if seeders exist and run seeders
-try {
-  execSync(
-    `npx sequelize db:seed:all --config "${sequelizeConfigPath}" --seeders-path "${seedersPath}"`
-  );
-  console.log("Seeders executed successfully.");
-} catch (error) {
-  console.log("Seeders not executed.");
-}
+// try {
+//   execSync(
+//     `npx sequelize db:seed:all --config "${sequelizeConfigPath}" --seeders-path "${seedersPath}"`
+//   );
+//   console.log("Seeders executed successfully.");
+// } catch (error) {
+//   console.log("Seeders not executed.");
+// }
 
 // Start your application using nodemon
-require("./api/index.js");
+

211d103d6ac9b076f85b1138efe7d12b3233ef8b
ach-fajarr <akhmadfajar223@gmail.com>
2024-02-10 14:01:16 +0700

    set security

--- Backend-Java/src/main/java/id/synrgy/travimate/config/WebSecurityConfig.java
+++ Backend-Java/src/main/java/id/synrgy/travimate/config/WebSecurityConfig.java
@@ -52,13 +52,13 @@ public class WebSecurityConfig {
                         auth
 //                            .requestMatchers("/api/v1/order/**").hasRole("user")
 //                            .requestMatchers("/api/v1/order/**").permitAll()
-                            .requestMatchers("/api/v1/flight/**").permitAll()
-                            .requestMatchers("/api/v1/payment/**").permitAll()
-
-                            .requestMatchers("/api-docs/**").permitAll()
-                            .requestMatchers("/swagger-ui/**").permitAll()
-                            .requestMatchers("/v3/api-docs/**").permitAll()
-                            .requestMatchers("/swagger-resources/**").permitAll()
+//                            .requestMatchers("/api/v1/flight/**").permitAll()
+//                            .requestMatchers("/api/v1/payment/**").permitAll()
+//
+//                            .requestMatchers("/api-docs/**").permitAll()
+//                            .requestMatchers("/swagger-ui/**").permitAll()
+//                            .requestMatchers("/v3/api-docs/**").permitAll()
+//                            .requestMatchers("/swagger-resources/**").permitAll()
                             .anyRequest().authenticated()
                         );
 

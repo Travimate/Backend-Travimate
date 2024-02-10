856ca59eb333915e21f48e6818429ac877ccc0b5
ach-fajarr <akhmadfajar223@gmail.com>
2024-02-10 23:15:23 +0700

    set security

--- Backend-Java/src/main/java/id/synrgy/travimate/model/User.java
+++ Backend-Java/src/main/java/id/synrgy/travimate/model/User.java
@@ -22,8 +22,8 @@ public class User {
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_roles",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id"))
+            joinColumns = @JoinColumn(name = "userId"),
+            inverseJoinColumns = @JoinColumn(name = "roleId"))
     private List<Role> roles;
 
 }

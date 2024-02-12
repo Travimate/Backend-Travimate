4b1aa6444404586dc5b7d5d958246c8ce7d6b583
ach-fajarr <akhmadfajar223@gmail.com>
2024-02-13 00:57:31 +0700

    update

--- Backend-Java/src/main/java/id/synrgy/travimate/config/WebConfig.java
+++ Backend-Java/src/main/java/id/synrgy/travimate/config/WebConfig.java
@@ -2,22 +2,16 @@ package id.synrgy.travimate.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }

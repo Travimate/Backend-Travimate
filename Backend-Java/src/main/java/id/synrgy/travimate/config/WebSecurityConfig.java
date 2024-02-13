5246c24e472ab777b4ab1bf937db3f6a6b5796e5
Akhmad Fajar Firman Syah <134356975+ach-fajarr@users.noreply.github.com>
2024-02-13 09:31:26 +0700

    Update WebSecurityConfig.java

--- Backend-Java/src/main/java/id/synrgy/travimate/config/WebSecurityConfig.java
+++ Backend-Java/src/main/java/id/synrgy/travimate/config/WebSecurityConfig.java
@@ -28,6 +28,8 @@ import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.Arrays;
+import java.util.List;
 
 import java.io.IOException;
 

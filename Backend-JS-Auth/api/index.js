e1ff84811568e7d339f0500ab595f3a538295724
bytadit <bgsp_aditya@student.uns.ac.id>
2024-02-15 20:56:14 +0700

    update deploy

--- Backend-JS-Auth/api/index.js
+++ Backend-JS-Auth/api/index.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(morgan("dev"));
 const PORT = process.env.PORT || 8081;
 
-// database
+// databas
 
 sequelize.sync().then(() => {
   console.log("Database synchronized");

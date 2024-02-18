183e409c621012afa070776f6457a914f97b6d5c
Akhmad Fajar Firman Syah <134356975+ach-fajarr@users.noreply.github.com>
2024-02-18 15:50:22 +0700

    Update DefaultMediaTypeConfig.java

--- Backend-Java/src/main/java/id/synrgy/travimate/config/DefaultMediaTypeConfig.java
+++ Backend-Java/src/main/java/id/synrgy/travimate/config/DefaultMediaTypeConfig.java
@@ -1,17 +1,17 @@
-package id.synrgy.travimate.config;
+// package id.synrgy.travimate.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.http.MediaType;
+// import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-@EnableWebMvc
-public class DefaultMediaTypeConfig implements WebMvcConfigurer {
+// @Configuration
+// @EnableWebMvc
+// public class DefaultMediaTypeConfig implements WebMvcConfigurer {
 
-    @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer.defaultContentType(MediaType.APPLICATION_JSON);
-    }
-}
+//     @Override
+//     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+//         configurer.defaultContentType(MediaType.APPLICATION_JSON);
+//     }
+// }

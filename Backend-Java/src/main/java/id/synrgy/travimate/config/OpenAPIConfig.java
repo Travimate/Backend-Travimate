08da2c6055c90ce4ccb796133e65aefef2d8e8ba
Akhmad Fajar Firman Syah <134356975+ach-fajarr@users.noreply.github.com>
2024-02-19 08:52:00 +0700

    Update OpenAPIConfig.java

--- Backend-Java/src/main/java/id/synrgy/travimate/config/OpenAPIConfig.java
+++ Backend-Java/src/main/java/id/synrgy/travimate/config/OpenAPIConfig.java
@@ -47,6 +47,10 @@ public class OpenAPIConfig {
         .license(mitLicense)
         .version("3.1.0");
 
-    return new OpenAPI().info(info).setOpenapi("3.0.0").servers(List.of(devServer, prodServer));
+    // return new OpenAPI().info(info).setOpenapi("3.0.0").servers(List.of(devServer, prodServer));
+    return new OpenAPI()
+    .info(info)
+    .setOpenapi("3.0.0")
+    .servers(List.of(devServer, prodServer));
   }
 }

cb43fb04ec751df7da71e6d9c470d344b4db9f6b
ach-fajarr <akhmadfajar223@gmail.com>
2024-02-21 20:52:50 +0700

    swagger fixing

--- Backend-Java/src/main/java/id/synrgy/travimate/config/DefaultMediaTypeConfig.java
+++ Backend-Java/src/main/java/id/synrgy/travimate/config/DefaultMediaTypeConfig.java
@@ -2,11 +2,9 @@ package id.synrgy.travimate.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,14 +13,12 @@ import java.util.List;
 @Configuration
 @EnableWebMvc
 public class DefaultMediaTypeConfig implements WebMvcConfigurer {
-
-    // @Override
-    // public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-    //     configurer.defaultContentType(MediaType.APPLICATION_JSON);
-    // }
-
-  @Override
+    @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        StringHttpMessageConverter messageConverter = new StringHttpMessageConverter();
+        messageConverter.setSupportedMediaTypes(List.of(MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN, MediaType.ALL));
+        converters.add(messageConverter);
         converters.add(new MappingJackson2HttpMessageConverter());
     }
+
 }
